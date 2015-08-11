6c3fe431161e477b1b0f77ad0ed7a2df3388c2dc
Tapasweni Pathak <tapaswenipathak@gmail.com>
2015-08-11 09:13:23 +0530

    pep8 fixes

--- horoscope/test.py
+++ horoscope/test.py
@@ -1,11 +1,10 @@
-sunsign=["aries","taurus","gemini","cancer","leo","virgo","libra","scorpio","stagittarius","capricorn","aquarius","pisces"]
-typ=["todays","weekly","yearly","monthly"]
-from  pyhoroscope import Horoscope as horo
+sunsign = ["aries", "taurus", "gemini", "cancer", "leo", "virgo",
+           "libra", "scorpio", "stagittarius", "capricorn", "aquarius", "pisces"]
+typ = ["todays", "weekly", "yearly", "monthly"]
+from pyhoroscope import Horoscope as horo
 from pprint import pprint
 for i in sunsign:
     for j in typ:
-        d=eval("horo.get_"+j+"_horoscope(i)")
-        d['horoscope']=d['horoscope'][:35]
+        d = eval("horo.get_" + j + "_horoscope(i)")
+        d['horoscope'] = d['horoscope'][:35]
         pprint(d)
-
-
