ceead16b340fdd9adf43627c763038bc638a2dd0
Tapasweni Pathak <tapaswenipathak@gmail.com>
2015-08-16 07:28:09 +0530

    updated to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='horoscope',
-    version='0.1.1',
+    version='0.1.2',
     description='Fetches and parses data from Ganeshaspeaks.',
     author='Tapasweni Pathak',
     author_email='tapaswenipathak@gmail.com',
