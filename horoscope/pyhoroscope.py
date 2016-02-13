cc90d4027f9578b8a1e90b3d52d246a8b649ca7e
Tapasweni Pathak <tapaswenipathak@gmail.com>
2016-02-13 17:25:05 +0530

    closes #8, not using bs4 only lxml

--- horoscope/pyhoroscope.py
+++ horoscope/pyhoroscope.py
@@ -1,8 +1,6 @@
 import urllib2
-from bs4 import BeautifulSoup
 from lxml import etree
-import ast
-
+import re
 
 ####################################################################
 # API
@@ -11,18 +9,8 @@ import ast
 
 '''
 If something breaks, give below error message.
-Horoscope will always be greater then THRESH.
 '''
 ERROR_MESSAGE = "Couldn't retrieve data, We are working to fix this."
-THRESH = 35
-
-'''
-Narrowest tag to fetch the horoscope and date.
-'''
-
-CLASS_NAME = "g5-box-25 first daily-hor sunsign-bx50"
-DATE_TAG = "soup.a"
-
 
 class Horoscope:
 
@@ -30,19 +18,15 @@ class Horoscope:
     def get_todays_horoscope(sunsign):
         url = "http://www.ganeshaspeaks.com/" + sunsign + \
             "/" + sunsign + "-daily-horoscope.action"
-        html_doc = urllib2.urlopen(url)
-        soup = BeautifulSoup(html_doc.read(), "html.parser")
-        raw_data = str(soup.find_all("div", CLASS_NAME))
-        soup = BeautifulSoup(raw_data, "html.parser")
-        horoscope = ERROR_MESSAGE
-        date = ""
-        if soup:
-            date = eval(DATE_TAG).string[2:-1]
-            for string in soup.stripped_strings:
-                if len(string) > THRESH:
-                    horoscope = string
-                    break
-
+        response = urllib2.urlopen(url)
+        htmlparser = etree.HTMLParser()
+        tree = etree.parse(response, htmlparser)
+        date = str(tree.xpath(
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[2]/div/div/div[1]/h3/span/text()"))
+        date = date.replace("['(", "").replace(")']", "")
+        horoscope = str(tree.xpath(
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[2]/div/div/div[1]/span/text()"))
+        horoscope = horoscope.replace("[u'", "").replace("']", "")
         dict = {
             'date': date,
             'horoscope': horoscope,
@@ -58,25 +42,26 @@ class Horoscope:
         htmlparser = etree.HTMLParser()
         tree = etree.parse(response, htmlparser)
         sanskrit_name = str(tree.xpath(
-            "/html/body/div/div/div[4]/section[4]/div/div/div[1]/p/text()[2]"))
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[10]/div[2]/div/span[2]/span/text()[2]"))
         sanskrit_name = sanskrit_name.replace(
-            "[\' : ", "").replace(" |\\n\']", "")
+            "[\' : ", "").replace(" |\\n\']", "").replace("['", "")
         meaning_of_name = str(tree.xpath(
-            "/html/body/div/div/div[4]/section[4]/div/div/div[1]/p/text()[3]"))
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[10]/div[2]/div/span[2]/span/text()[3]"))
         meaning_of_name = meaning_of_name.replace(
-            "[\' : ", "").replace(" |\\n\']", "")
+            "[\' : ", "").replace(" |\\n\']", "").replace("['", "")
         lord = str(tree.xpath(
-            "/html/body/div/div/div[4]/section[4]/div/div/div[1]/p/text()[5]"))
-        lord = lord.replace("[\' : ", "").replace(" |\\n\']", "")
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[10]/div[2]/div/span[2]/span/text()[5]"))
+        lord = lord.replace("[\' : ", "").replace(" |\\n\']", "").replace("['", "")
         Type = str(tree.xpath(
-            "/html/body/div/div/div[4]/section[4]/div/div/div[1]/p/text()[4]"))
-        Type = Type.replace("[\' : ", "").replace(" |\\n\']", "")
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[10]/div[2]/div/span[2]/span/text()[4]"))
+        Type = Type.replace("[\' : ", "").replace(" |\\n\']", "").replace("['", "").replace(" |']", "")
         lucky_day = str(tree.xpath(
-            "/html/body/div/div/div[4]/section[4]/div/div/div[1]/p/text()[6]"))
-        lucky_day = lucky_day.replace("[\' : ", "").replace(" |\\n\']", "")
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[10]/div[2]/div/span[2]/span/text()[6]"))
+        lucky_day = lucky_day.replace("[\' : ", "").replace(" |\\n\']", "").replace("[' ", "")
         lucky_number = str(tree.xpath(
-            "/html/body/div/div/div[4]/section[4]/div/div/div[1]/p/text()[7]"))
-        lucky_number = lucky_number.replace("[\' : ", "").replace("\\n']", "")
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[10]/div[2]/div/span[2]/span/text()"))
+        lucky_number = str(re.findall(r"\d+,", lucky_number))
+        lucky_number = lucky_number.replace("['", "").replace(",']", "").replace("'", "").replace(",,", ",")
         dict = {
             'sanskrit_name': sanskrit_name,
             'meaning_of_name': meaning_of_name,
@@ -91,21 +76,17 @@ class Horoscope:
     def get_weekly_horoscope(sunsign):
         url = "http://www.ganeshaspeaks.com/" + sunsign + \
             "/" + sunsign + "-weekly-horoscope.action"
-        html_doc = urllib2.urlopen(url)
-        soup = BeautifulSoup(html_doc.read(), "html.parser")
-        raw_data = str(soup.find_all("div", CLASS_NAME))
-        soup = BeautifulSoup(raw_data, "html.parser")
-        horoscope = ERROR_MESSAGE
-        week = ""
-        if soup:
-            week = eval(DATE_TAG).string[2:-2]
-            for string in soup.stripped_strings:
-                if len(string) > THRESH:
-                    horoscope = string
-                    break
-
+        response = urllib2.urlopen(url)
+        htmlparser = etree.HTMLParser()
+        tree = etree.parse(response, htmlparser)
+        date = str(tree.xpath(
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[2]/div/div/h3/span/text()"))
+        date = date.replace("[u'\\n", "").replace("']", "").replace("\\u2013", "-")
+        horoscope = str(tree.xpath(
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[2]/div/div/span/text()"))
+        horoscope = horoscope.replace("['", "").replace("']", "")
         dict = {
-            'week': week,
+            'date': date,
             'horoscope': horoscope,
             'sunsign': sunsign
         }
@@ -116,21 +97,17 @@ class Horoscope:
     def get_monthly_horoscope(sunsign):
         url = "http://www.ganeshaspeaks.com/" + sunsign + \
             "/" + sunsign + "-monthly-horoscope.action"
-        html_doc = urllib2.urlopen(url)
-        soup = BeautifulSoup(html_doc.read(), "html.parser")
-        raw_data = str(soup.find_all("div", CLASS_NAME))
-        soup = BeautifulSoup(raw_data, "html.parser")
-        horoscope = ERROR_MESSAGE
-        month = ""
-        if soup:
-            month = eval(DATE_TAG).string[2:-2]
-            for string in soup.stripped_strings:
-                if len(string) > THRESH:
-                    horoscope = string
-                    break
-
+        response = urllib2.urlopen(url)
+        htmlparser = etree.HTMLParser()
+        tree = etree.parse(response, htmlparser)
+        date = str(tree.xpath(
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[2]/div/div/h3/span/text()"))
+        date = date.replace("['\\n", "").replace("']", "")
+        horoscope = str(tree.xpath(
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[2]/div/div/span/text()[1]"))
+        horoscope = horoscope.replace("['", "").replace("']", "")
         dict = {
-            'month': month,
+            'date': date,
             'horoscope': horoscope,
             'sunsign': sunsign
         }
@@ -141,21 +118,17 @@ class Horoscope:
     def get_yearly_horoscope(sunsign):
         url = "http://www.ganeshaspeaks.com/" + sunsign + \
             "/" + sunsign + "-yearly-horoscope.action"
-        html_doc = urllib2.urlopen(url)
-        soup = BeautifulSoup(html_doc.read(), "html.parser")
-        raw_data = str(soup.find_all("div", CLASS_NAME))
-        soup = BeautifulSoup(raw_data, "html.parser")
-        horoscope = ERROR_MESSAGE
-        year = ""
-        if soup:
-            year = eval(DATE_TAG).string[2:-2]
-            for string in soup.stripped_strings:
-                if len(string) > THRESH:
-                    horoscope = string
-                    break
-
+        response = urllib2.urlopen(url)
+        htmlparser = etree.HTMLParser()
+        tree = etree.parse(response, htmlparser)
+        date = str(tree.xpath(
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[2]/div/div/h3/span/text()"))
+        date = date.replace("['\\n", "").replace("']", "")
+        horoscope = str(tree.xpath(
+            "//*[@id=\"main-wrapper\"]/div/div/div[2]/section/div[2]/div/div/span/text()"))
+        horoscope = horoscope.replace("[u'", "").replace("']", "").replace("\\xe2\\x80\\x99s", "")
         dict = {
-            'year': year,
+            'date': date,
             'horoscope': horoscope,
             'sunsign': sunsign
         }
