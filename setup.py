918f0099e95e127ed776ab7f91a8a5230ac65c4a
Tapasweni Pathak <tapaswenipathak@gmail.com>
2016-05-28 14:40:58 +0530

    not using bs4

--- setup.py
+++ setup.py
@@ -2,13 +2,10 @@ from setuptools import setup
 
 setup(
     name='horoscope',
-    version='0.1.2',
+    version='1.0.1',
     description='Fetches and parses data from Ganeshaspeaks.',
     author='Tapasweni Pathak',
     author_email='tapaswenipathak@gmail.com',
     url='https://github.com/tapasweni-pathak/pyhoroscope',
     packages=['horoscope'],
-    install_requires=[
-        "beautifulsoup4 == 4.3.2",
-    ]
     )
